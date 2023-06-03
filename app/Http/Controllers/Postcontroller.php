25a0dae186fb941469083afff888e133a02f6e45
yuuichino <y.ichino0420@gmail.com>
2023-06-03 20:51:54 +0900

    08-1

---
+++ app/Http/Controllers/Postcontroller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+/**
+	*  * Post一覧を表示する
+	*   * 
+	*    * @param Post Postモデル
+	*     * @return array Postモデルリスト
+	*      */
+
+class Postcontroller extends Controller
+{
+	//
+
+	public function index(Post $post)
+	{
+		    return $post->get();
+	}
+}
