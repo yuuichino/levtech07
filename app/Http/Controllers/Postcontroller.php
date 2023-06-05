f1929b7ab48811cfe1016a2c5cc912af8547e067
yuuichino <y.ichino0420@gmail.com>
2023-06-05 23:54:22 +0900

    08-2

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -17,7 +17,7 @@ class Postcontroller extends Controller
 
 	public function index(Post $post)
 	{
-		    return view('posts.index') ->with(['posts' => $post->getByLimit()]);
+		    return view('posts.index') ->with(['posts' => $post->getPaginateByLimit()]);
 	}
 }
 ?>
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,4 +15,8 @@ public function getByLimit(int $limit_count = 10)
 	//updated_atで降順に並べた後、limitで件数制限かける
 	return $this->orderBy('updated_at','DESC')->limit($limit_count)->get();
 }
+
+public function getPaginateByLimit(int $limit_count = 5)
+{ return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+}
 }
