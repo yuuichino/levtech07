95ace1f2440c997f2a25429e3510a41bbc1e76a1
yuuichino <y.ichino0420@gmail.com>
2023-06-13 01:30:10 +0900

    08-4-4

--- app/Http/Controllers/Postcontroller.php
+++ app/Http/Controllers/Postcontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 /**
 	*  * Post一覧を表示する
@@ -30,7 +31,7 @@ class Postcontroller extends Controller
 		return view('posts/create');
 	}
 	
-	public function store(Request $request, Post $post)
+	public function store(PostRequest $request, Post $post)
 	{
 		$input = $request['post'];
 		$post->fill($input)->save();
