d2409a97e097a32148ba4e149a1c9c40ba9b0442
yuuichino <y.ichino0420@gmail.com>
2023-06-15 18:49:56 +0900

    08-6

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -30,7 +30,7 @@
         'use strict'
         
         if(confirm('削除すると復元できません。\n本当に削除しますか？')){
-            document.getElementById('form_${id}').submit();
+            document.getElementById(`form_${id}`).submit();
         }
     }
 </script>
